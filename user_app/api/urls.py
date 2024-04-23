7ee4431f5d3455bb8fb702d8bdfde9db05ab99bd
Sherif ABouzeid <sherif.abouzeid97@gmail.com>
2024-04-23 20:40:13 +0200

    login/register

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from user_app.api.views import UserAV
+from user_app.api.views import RegistrationView, logout
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
-    path('register/', UserAV.as_view(), name='register'),
-
+    path('register/', RegistrationView, name='register'),
+    path('logout/', logout, name='logout'),
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,7 +1,31 @@
 from user_app.api.serializers import UserSerializer
 from rest_framework import generics
-from user_app.api.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
+from user_app import models
 
-class UserAV(generics.CreateAPIView):
-    serializer_class = UserSerializer
+@api_view(['POST',])
+def logout(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response({'Response': 'Logout Successfully'})
+
+@api_view(['POST',])
+def RegistrationView(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        data = {} 
+        if serializer.is_valid():
+            account = serializer.save()
+            data['Response'] = "Registration Successfully"
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+        else:
+            data = serializer.errors
+        return Response(data)
+# class UserAV(generics.CreateAPIView):
+#     serializer_class = UserSerializer
     
